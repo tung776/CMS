19fd8af4ac32a5c3ff7e64c8a7e31559506707ca
nguyễn Thanh Tùng <thanhtung776@gmail.com>
2018-12-19 15:00:26 +0700

    config bublelint

--- client/app/src/views/Home.vue
+++ client/app/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import axios from 'axios'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  mounted() {
+    axios.get('http://localhost:1000/vehicles').then(response => {
+      console.log(response);
+    })
+  },
+  
 }
 </script>
